22d036237eb07feea89d21fabd89544f92a23994
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-19 11:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let str=s1.toLowerCase();
+ let str2=s2.toLowerCase();
+  for(let i=0;i<str.length;i++){
+    let str1=str.substr(i,(str2.length));
+    if(str1===str2){
+      return i;
+    }
+  }
+  return -1;
 }
-
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
